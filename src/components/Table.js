a06af0d408e66d192e791e1ce7e394bde0686f58
крис <krissspik@gmail.com>
2026-06-06 03:02:37 +1000

    4th lab for web

---
+++ src/components/Table.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import TableHead from './TableHead.js';
+import TableBody from './TableBody.js';
+import '../CSS/index.css';
+import '../CSS/App.css';
+import Filter from './Filter.js';
+
+const Table = (props) => {
+
+    const showPag = props.showPag === true;
+
+    const [activePage, setActivePage] = useState("1");
+
+    const changeActive = (event) => {
+        setActivePage(event.target.innerHTML);
+    };
+
+    const [dataTable, setDataTable] = useState(props.data);
+
+    const updateDateTable = (value) => {
+        setDataTable(value);
+        setActivePage("1");
+    };
+
+    const amountRows = showPag ? Number(props.amountRows) : dataTable.length;
+
+    const totalPages = showPag ? Math.ceil(dataTable.length / props.amountRows) : 1;
+    const arr = Array.from({ length: totalPages }, (v, i) => i + 1);
+
+    const pages = arr.map((item, index) => (
+        <span
+            key={index}
+            className={Number(activePage) === item ? "active" : "1"}
+            onClick={changeActive}
+        >
+            {item}
+        </span>
+    ));
+
+    return (
+        <>
+            <h4>Фильтры</h4>
+            <Filter
+                filtering={updateDateTable}
+                data={dataTable}
+                fullData={props.data}
+            />
+
+            <div className="table-container">
+                <table>
+                    <TableHead head={Object.keys(props.data[0])} />
+                    <TableBody
+                        body={dataTable}
+                        amountRows={amountRows}
+                        numPage={showPag ? activePage : 1}
+                    />
+                </table>
+
+                {showPag && totalPages > 1 && (
+                    <div id="pag">
+                        {pages}
+                    </div>
+                )}
+            </div>
+        </>
+    );
+};
+
+export default Table;
